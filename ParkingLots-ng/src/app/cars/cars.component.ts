dd10ec69b0fab8dcc6953937f2606f534c338c91
rbaas1 <rbaas1@gmail.com>
2017-11-09 12:19:16 +0100

    Added random car generator

--- ParkingLots-ng/src/app/cars/cars.component.ts
+++ ParkingLots-ng/src/app/cars/cars.component.ts
@@ -21,8 +21,34 @@ export class CarsComponent implements OnInit {
   }
 
   makeCar(){
-    console.log("attempting to create car");
-    this.carService.saveCar("twertwer", "#888eeF");
+    this.carService.saveCar(this.generateLicensePlate(), this.generateColour());
+  }
+
+  //TODO: make into function instead of having to use this.
+  generateLicensePlate(): string{
+    var possibleLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    var possibleNumbers = "0123456789";
+    return "" + possibleLetters.charAt(Math.floor(Math.random() * possibleLetters.length))
+              + possibleLetters.charAt(Math.floor(Math.random() * possibleLetters.length))
+              + "-"
+              + possibleLetters.charAt(Math.floor(Math.random() * possibleLetters.length))
+              + possibleLetters.charAt(Math.floor(Math.random() * possibleLetters.length))
+              + "-"
+              + possibleNumbers.charAt(Math.floor(Math.random() * possibleNumbers.length))
+              + possibleNumbers.charAt(Math.floor(Math.random() * possibleNumbers.length));
+
+  }
+
+  generateColour(): string{
+    return this.rgbToHex(Math.floor(Math.random() * 255), Math.floor(Math.random() * 255), Math.floor(Math.random() * 255));
+  }
+
+  rgbToHex(r, g, b) {
+    return "#" + this.componentToHex(r) + this.componentToHex(g) + this.componentToHex(b);
+  }
+  componentToHex(c) {
+    var hex = c.toString(16);
+    return hex.length == 1 ? "0" + hex : hex;
   }
 
 }
